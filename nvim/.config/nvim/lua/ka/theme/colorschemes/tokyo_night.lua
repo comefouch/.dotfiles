c46cfbb9ddcb259eb800a8b0d6f10cf71d897da7
come <c.delafouchardiere@otaree.com>
2022-04-29 20:42:03 +0200

    fix(neovim): Fix transparency in kitty for tokyo_night

--- nvim/.config/nvim/lua/ka/theme/colorschemes/tokyo_night.lua
+++ nvim/.config/nvim/lua/ka/theme/colorschemes/tokyo_night.lua
@@ -6,8 +6,16 @@ vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_transparent_background = false
 vim.g.tokyonight_transparent = false
 
-vim.cmd('highlight ColorColumn ctermbg=0 guibg=lightgrey')
-vim.cmd('highlight SignColumn guibg=#282828')
-vim.cmd('highlight Normal guibg=#282828')
-
 vim.cmd('colorscheme tokyonight')
+
+vim.cmd('highlight ColorColumn ctermbg=Black guibg=lightgrey')
+vim.cmd('highlight SignColumn ctermbg=Black guibg=#1A1A26')
+vim.cmd('highlight Normal ctermbg=Black guibg=#1A1A26')
+vim.cmd('highlight NormalNC ctermbg=Black guibg=#1A1A26')
+vim.cmd('highlight LineNr ctermbg=Black guibg=#1A1A26')
+vim.cmd('highlight Folded ctermbg=Black guibg=#1A1A26')
+vim.cmd('highlight NonText ctermbg=Black guibg=#1A1A26')
+vim.cmd('highlight SpecialKey ctermbg=Black guibg=#1A1A26')
+vim.cmd('highlight VertSplit ctermbg=Black guibg=#1A1A26')
+vim.cmd('highlight EndOfBuffer ctermbg=Black guibg=#1A1A26')
+
